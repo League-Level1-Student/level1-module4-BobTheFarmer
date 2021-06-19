39e2db71fdfc0dc826396e0e1e17c99b93bcecef
league <league@imac-38.attlocal.net>
2021-06-18 17:00:17 -0700

    Almost done w/ caculator

--- src/_07_fortune_teller/Magic8Ball.java
+++ src/_07_fortune_teller/Magic8Ball.java
@@ -7,18 +7,30 @@ package _07_fortune_teller;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below….
-
+		public static void main(String[] args) {
+			
+		
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+			int random = new Random().nextInt(4);
 	// 3. Print out this variable
-
+			System.out.println(random);
 	// 4. Get the user to enter a question for the 8 ball
-
+			JOptionPane.showInputDialog(null, "What is your magic 8 ball question?");
 	// 5. If the random number is 0
-
+			if(random == 0) {
+				JOptionPane.showMessageDialog(null, "Yes");
+			} else if(random==1) {
+				JOptionPane.showMessageDialog(null, "No");
+			} else if(random==2) {
+				JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+			} else {
+				JOptionPane.showMessageDialog(null, "Pet a dog");
+			}
 	// -- tell the user "Yes"
 
 	// 6. If the random number is 1
@@ -32,5 +44,5 @@ public class Magic8Ball {
 	// 8. If the random number is 3
 
 	// -- write your own answer
-
+		}
 }
---
+++ src/_08_calculator/Calculator.java
@@ -0,0 +1,87 @@
+package _08_calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+	
+
+
+public class Calculator implements ActionListener {
+	JButton add; 
+	JButton sub; 
+	JButton mul; 
+	JButton div; 
+	JTextField num1;
+	JTextField num2;
+	JLabel label;
+	void run() {
+		//Make UI
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		frame.setVisible(true);
+		num1 = new JTextField(12);
+		num2 = new JTextField(12);
+		add = new JButton("add");
+		sub = new JButton("sub");
+		mul = new JButton("mul");
+		div = new JButton("div");
+		panel.add(num1);
+		panel.add(num2);
+		panel.add(add);
+		panel.add(sub);
+		panel.add(mul);
+		panel.add(div);
+		label = new JLabel("Do Math");
+		panel.add(label);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(350, 150);
+		
+		add.addActionListener(this);
+		sub.addActionListener(this);
+		mul.addActionListener(this);
+		div.addActionListener(this);
+		
+	}
+	
+	
+	
+	
+	double add(double num1,double num2) {
+		return(num1+num2);
+	}
+	double sub(double num1,double num2) {
+		return(num1-num2);
+	}
+	double mul(double num1,double num2) {
+		return(num1*num2);
+	}
+	double div(double num1,double num2) {
+		return(num1/num2);
+	}
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(num1.getText() == null || num2.getText() == null) {
+			label.setText("ERROR");
+		} else if(e.getSource() == add) {
+			label.setText(String.valueOf(add(Double.parseDouble(num1.getText(), Double.parseDouble(num2.getText())))));
+			//WORK ON THIS AND COPY AND PASTE INTO OTHER OPERATORS
+		} else if(e.getSource() == sub) {
+			
+		} else if(e.getSource() == mul) {
+			
+		} else {
+			
+		}
+		
+	}
+}
