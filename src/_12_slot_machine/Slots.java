8b9bdfe160b951486f53aa5a0e835ad3c35db9fd
league <league@imac-109.attlocal.net>
2021-07-15 18:28:19 -0700

    Finished Module

--- src/_12_slot_machine/Slots.java
+++ src/_12_slot_machine/Slots.java
@@ -9,23 +9,37 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Slots implements ActionListener {
 	Random rando;
-	
-	int image1;
-	int image2;
-	int image3;
+	Icon icon;
+	JLabel label1 = new JLabel(); 
+	JLabel label2 = new JLabel(); 
+	JLabel label3 = new JLabel(); 
+	String image1;
+	String image2;
+	String image3;
 	void run() {
 		//Make UI+Classes
 			JPanel panel = new JPanel();
 			JFrame frame = new JFrame();
+			frame.add(panel);
 			frame.setVisible(true);
+			frame.setSize(700, 300);
 			rando = new Random();
 			JButton spin = new JButton("Spin");
 			spin.addActionListener(this);
+			panel.add(label1);
+			panel.add(label2);
+			panel.add(label3);
+			panel.add(spin);
+			frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 			
+			//Run
+				spin();
 	}
 	
 	
@@ -41,7 +55,7 @@ public class Slots implements ActionListener {
 		System.err.println("Could not find image " + fileName);
 		return null;
 	}
-	Icon icon = new ImageIcon(imageURL);
+	icon = new ImageIcon(imageURL);
 	return icon;
 }
 
@@ -54,13 +68,27 @@ public class Slots implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//Change varibles
-		
-		
+		spin();
 	}
 	
-	void UpdateImages() {
-		
-		
+	void spin() {
+		//Change varibles
+			image1 = rando.nextInt(3) + ".jpg";
+			image2 = rando.nextInt(3) + ".jpg";
+			image3 = rando.nextInt(3) + ".jpg";
+			
+		//Set Icons
+			
+			icon = createLabelIcon(image1);
+			label1.setIcon(icon);
+			icon = createLabelIcon(image2);
+			label2.setIcon(icon);
+			icon = createLabelIcon(image3);
+			label3.setIcon(icon);
+			
+		//Check if won
+			if(image1.equals(image2) && image1.equals(image3)) {
+				JOptionPane.showMessageDialog(null, "You Won! Take nothing as a reward");
+			}
 	}
 }
